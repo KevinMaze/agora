17ad5c5c60ba60e2a3b0d858f09aca9a29d6ab95
Kévin MAZE <kekeproject@hotmail.fr>
2025-10-10 19:18:12 +0200

    finish footer

--- src/ui/components/footer/footer.tsx
+++ src/ui/components/footer/footer.tsx
@@ -4,24 +4,94 @@ import Colonne from "@/../public/assets/images/colonne.png";
 import { Typo } from "@/ui/design-system/typography";
 import { Logo } from "@/ui/design-system/logo";
 import Link from "next/link";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div className=" bg-amber-300">
-            <Container className="flex justify-between">
-                <div className=" bg-amber-600">
-                    <Image src={Colonne} alt="Colonne gauche" width={150} />
-                </div>
-                <div className="">
+        <div className="relative overflow-hidden bg-background shadow-[var(--myshadow)]">
+            <div className="absolute top-0 bottom-0 left-0 w-[125px]">
+                <Image
+                    src={Colonne}
+                    alt="Colonne gauche"
+                    fill
+                    className="object-cover"
+                />
+            </div>
+            <Container>
+                <div className="items-center flex flex-col gap-6 py-10">
                     <Link href="/">
                         <Logo size="large" />
                     </Link>
-                    <Typo>Bienvenue</Typo>
-                </div>
-                <div className="bg-amber-600">
-                    <Image src={Colonne} alt="Colonne droite" width={150} />
+
+                    <Typo
+                        variant="para"
+                        color="primary"
+                        className="text-[16px] uppercase"
+                        weight="bold"
+                    >
+                        <Link href="/">Accueil</Link> -{" "}
+                        <Link href="/coffeeshop">Coffee Shop</Link> -{" "}
+                        <Link href="/bibliotheque">La Bibliothèque</Link> -{" "}
+                        <Link href="/librairie">La Librairie</Link> -{" "}
+                        <Link href="/gazette">La Gazette</Link> -{" "}
+                        <Link href="/collect">Click and Collect</Link> -{" "}
+                        <Link href="/apropos">A propos</Link>
+                    </Typo>
+
+                    <Typo
+                        variant="para"
+                        color="secondary"
+                        className="flex gap-4 text-[16px]"
+                    >
+                        Suivez l`Agora sur
+                        <Link
+                            href="https://www.instagram.com"
+                            target="_blank"
+                            className="flex gap-1"
+                        >
+                            <FaInstagram />
+                            Instagram
+                        </Link>
+                        <Link
+                            href="https://www.facebook.com"
+                            target="_blank"
+                            className="flex gap-1"
+                        >
+                            <FaFacebook /> Facebook
+                        </Link>
+                        <Link
+                            href="https://www.twitter.com"
+                            target="_blank"
+                            className="flex gap-1"
+                        >
+                            <FaXTwitter /> Twitter
+                        </Link>
+                    </Typo>
+                    <div className="border-1 border-secondary w-100"></div>
+                    <Typo
+                        variant="para"
+                        className="flex gap-2 text-[16px]"
+                        color="secondary"
+                    >
+                        {`Copyright © ${currentYear} Agora`} -{" "}
+                        <Link href="/mentions-legales">Mentions légales</Link> -{" "}
+                        <Link href="/politique-de-confidentialite">
+                            Politique de confidentialité
+                        </Link>
+                    </Typo>
                 </div>
             </Container>
+            <div className="absolute top-0 bottom-0 right-0 w-[125px]">
+                <Image
+                    src={Colonne}
+                    alt="Colonne droite"
+                    fill
+                    className="object-cover"
+                />
+            </div>
         </div>
     );
 };
