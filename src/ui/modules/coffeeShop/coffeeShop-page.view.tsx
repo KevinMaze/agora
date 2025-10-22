7a24e0b992b29f3c05f0ebfa5782a74580309f28
Kévin MAZE <kekeproject@hotmail.fr>
2025-10-22 18:17:44 +0200

    add header from coffe shop page

--- src/ui/modules/coffeeShop/coffeeShop-page.view.tsx
+++ src/ui/modules/coffeeShop/coffeeShop-page.view.tsx
@@ -1,3 +1,9 @@
+import { Header } from "./component/header.view";
+
 export const CoffeeShopView = () => {
-    return <>hello</>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 };
