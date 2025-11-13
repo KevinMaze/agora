19df42c874d9a2df3e6554bc35345ac5aba95052
Kévin MAZE <kekeproject@hotmail.fr>
2025-11-13 17:20:27 +0100

    add section avis

--- src/ui/modules/librairie-page/components/like-bookbox.view.tsx
+++ src/ui/modules/librairie-page/components/like-bookbox.view.tsx
@@ -5,6 +5,9 @@ import React, { useMemo } from "react";
 import DefaultImage from "@/../public/assets/images/404.png"; // Image par défaut
 import Dune from "@/../public/assets/images/dune.jpg";
 import { Button } from "@/ui/design-system/button";
+import { BiArrowFromLeft } from "react-icons/bi";
+import { BsArrowBarLeft, BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import { Avatar } from "@/ui/design-system/avatar";
 
 interface LikeBookBoxViewProps {
     title?: string;
@@ -72,97 +75,225 @@ export const LikeBookBoxView: React.FC<LikeBookBoxViewProps> = ({
     };
 
     return (
-        <Container className="py-20 border-b-2 border-primary ">
-            <Typo
-                variant="title"
-                components="h2"
-                weight="bold"
-                color="primary"
-                className="mb-10 uppercase text-4xl text-end"
-            >
-                Coup de coeur de la boite aux livres
-            </Typo>
-            <div className="p-10 bg-foreground mx-auto h-full rounded-lg shadow-lg max-w-6xl">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-full overflow-y-auto">
-                    {/* Colonne de l'image */}
-                    <div className="relative border-2 border-primary rounded-lg">
-                        <Image
-                            src={Dune}
-                            alt={alt ?? "book cover"}
-                            className="rounded-lg object-cover w-full h-full "
-                        />
-                        <div className="absolute bottom-2 left-2 bg-foreground bg-opacity-75 px-4 py-2 rounded-md flex items-center">
-                            <Typo
-                                variant="para"
-                                components="p"
-                                weight="bold"
-                                color="primary"
-                                className="text-xl"
-                            >
-                                Note des lecteurs :
-                            </Typo>
-                            <StarRating rating={averageRating} />
+        <>
+            <Container className="py-20 border-b-2 border-primary ">
+                <Typo
+                    variant="title"
+                    components="h2"
+                    weight="bold"
+                    color="primary"
+                    className="mb-10 uppercase text-4xl text-end"
+                >
+                    Coup de coeur de la boite aux livres
+                </Typo>
+                <div className="p-10 bg-foreground mx-auto h-full rounded-lg shadow-lg max-w-6xl">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-full overflow-y-auto">
+                        {/* Colonne de l'image */}
+                        <div className="relative border-2 border-primary rounded-lg">
+                            <Image
+                                src={Dune}
+                                alt={alt || "Book Cover"}
+                                className="rounded-lg object-cover w-full h-full "
+                            />
+                            <div className="absolute bottom-2 left-1/2 -translate-x-1/2 bg-foreground/80 px-4 py-2 rounded-md">
+                                <Typo
+                                    variant="para"
+                                    components="p"
+                                    weight="bold"
+                                    color="primary"
+                                    className="text-xl"
+                                >
+                                    Note des lecteurs :
+                                </Typo>
+                                <StarRating rating={averageRating} />
+                            </div>
+                        </div>
+                        {/* Colonne des détails */}
+                        <div className="space-y-5 flex flex-col">
+                            <div>
+                                <Typo
+                                    variant="title"
+                                    components="h2"
+                                    weight="bold"
+                                    color="primary"
+                                    className="uppercase text-3xl underline "
+                                >
+                                    Titre
+                                </Typo>
+                                <Typo
+                                    variant="para"
+                                    color="other"
+                                    className="mt-2 text-xl"
+                                >
+                                    Dune
+                                </Typo>
+                            </div>
+
+                            <div>
+                                <Typo
+                                    variant="title"
+                                    components="h2"
+                                    weight="bold"
+                                    color="primary"
+                                    className="mb-2 uppercase text-3xl underline"
+                                >
+                                    Date de parution
+                                </Typo>
+                                <Typo
+                                    variant="para"
+                                    color="other"
+                                    className="mt-2 text-xl"
+                                >
+                                    1965
+                                </Typo>
+                            </div>
+
+                            <div>
+                                <Typo
+                                    variant="title"
+                                    components="h2"
+                                    weight="bold"
+                                    color="primary"
+                                    className="mb-2 uppercase text-3xl underline"
+                                >
+                                    Auteur
+                                </Typo>
+                                <Typo
+                                    variant="para"
+                                    color="other"
+                                    className="mt-2 text-xl"
+                                >
+                                    Frank Herbert
+                                </Typo>
+                            </div>
+                            <div>
+                                <Typo
+                                    variant="title"
+                                    components="h2"
+                                    weight="bold"
+                                    color="primary"
+                                    className="mb-2 uppercase text-3xl underline"
+                                >
+                                    Synopsis
+                                </Typo>
+                                <Typo
+                                    variant="para"
+                                    color="other"
+                                    className="mt-2 text-xl"
+                                >
+                                    Set in the distant future amidst a huge
+                                    interstellar empire, Dune tells the story of
+                                    young Paul Atreides, whose noble family
+                                    accepts the stewardship of the desert planet
+                                    Arrakis. While the planet is an inhospitable
+                                    and sparsely populated world, it is the only
+                                    source of melange, or &quot;the spice&quot;,
+                                    a drug that extends life and enhances mental
+                                    abilities. As melange is the most valuable
+                                    substance in the universe, control of
+                                    Arrakis is a coveted and dangerous
+                                    undertaking.
+                                </Typo>
+                            </div>
                         </div>
                     </div>
-                    {/* Colonne des détails */}
-                    <div className="space-y-5 flex flex-col">
-                        <div>
-                            <Typo
-                                variant="title"
-                                components="h2"
-                                weight="bold"
-                                color="primary"
-                                className="uppercase text-3xl underline "
-                            >
-                                Titre
-                            </Typo>
-                            <Typo
-                                variant="para"
-                                color="other"
-                                className="mt-2 text-xl"
-                            >
-                                Dune
-                            </Typo>
-                        </div>
+                    <div className="mt-10 flex justify-end">
+                        <Button
+                            variant="primary"
+                            size="large"
+                            icon={{ icon: BsArrowRight }}
+                            iconPosition="left"
+                            action={() => {
+                                console.log("Emprunter le livre");
+                            }}
+                        >
+                            Emprunter
+                        </Button>
+                    </div>
+                </div>
+            </Container>
 
-                        <div>
-                            <Typo
-                                variant="title"
-                                components="h2"
-                                weight="bold"
-                                color="primary"
-                                className="mb-2 uppercase text-3xl underline"
-                            >
-                                Date de parution
-                            </Typo>
-                            <Typo
-                                variant="para"
-                                color="other"
-                                className="mt-2 text-xl"
-                            >
-                                1965
-                            </Typo>
-                        </div>
+            <Container className="py-20 border-b-2 border-primary ">
+                <Typo
+                    variant="title"
+                    components="h2"
+                    weight="bold"
+                    color="primary"
+                    className="mb-10 uppercase text-4xl text-end"
+                >
+                    Derniers avis des lecteurs
+                </Typo>
+                <div className="p-10 bg-foreground mx-auto h-full rounded-lg shadow-lg max-w-6xl">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-full overflow-y-auto">
+                        {/* Colonne de l'image */}
 
-                        <div>
-                            <Typo
-                                variant="title"
-                                components="h2"
-                                weight="bold"
-                                color="primary"
-                                className="mb-2 uppercase text-3xl underline"
-                            >
-                                Auteur
-                            </Typo>
-                            <Typo
-                                variant="para"
-                                color="other"
-                                className="mt-2 text-xl"
-                            >
-                                Frank Herbert
-                            </Typo>
+                        {/* Colonne des détails */}
+                        <div className="space-y-5 flex flex-col">
+                            <div>
+                                <Typo
+                                    variant="title"
+                                    components="h2"
+                                    weight="bold"
+                                    color="primary"
+                                    className="uppercase text-3xl underline "
+                                >
+                                    Nom / pseudo
+                                </Typo>
+                                <div className="flex items-center gap-4 mt-2">
+                                    <Typo
+                                        variant="para"
+                                        color="other"
+                                        className="mt-2 text-xl"
+                                    >
+                                        Jean Dupont
+                                    </Typo>
+                                    <Avatar
+                                        size="small"
+                                        src={DefaultImage}
+                                        alt="Avatar de Jean Dupont"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <Typo
+                                    variant="title"
+                                    components="h2"
+                                    weight="bold"
+                                    color="primary"
+                                    className="mb-2 uppercase text-3xl underline"
+                                >
+                                    Date de publication
+                                </Typo>
+                                <Typo
+                                    variant="para"
+                                    color="other"
+                                    className="mt-2 text-xl"
+                                >
+                                    2025-01-15
+                                </Typo>
+                            </div>
+
+                            <div>
+                                <Typo
+                                    variant="title"
+                                    components="h2"
+                                    weight="bold"
+                                    color="primary"
+                                    className="mb-2 uppercase text-3xl underline"
+                                >
+                                    Nome du livre
+                                </Typo>
+                                <Typo
+                                    variant="para"
+                                    color="other"
+                                    className="mt-2 text-xl"
+                                >
+                                    Dune
+                                </Typo>
+                            </div>
                         </div>
-                        <div>
+                        <div className=" space-y-5 flex flex-col">
                             <Typo
                                 variant="title"
                                 components="h2"
@@ -170,32 +301,30 @@ export const LikeBookBoxView: React.FC<LikeBookBoxViewProps> = ({
                                 color="primary"
                                 className="mb-2 uppercase text-3xl underline"
                             >
-                                Synopsis
+                                Avis
                             </Typo>
                             <Typo
                                 variant="para"
                                 color="other"
                                 className="mt-2 text-xl"
                             >
-                                Set in the distant future amidst a huge
-                                interstellar empire, Dune tells the story of
-                                young Paul Atreides, whose noble family accepts
-                                the stewardship of the desert planet Arrakis.
-                                While the planet is an inhospitable and sparsely
-                                populated world, it is the only source of
-                                melange, or &quot;the spice&quot;, a drug that
-                                extends life and enhances mental abilities. As
-                                melange is the most valuable substance in the
-                                universe, control of Arrakis is a coveted and
-                                dangerous undertaking.
+                                Un chef-d'œuvre de la science-fiction qui
+                                explore des thèmes profonds tels que la
+                                politique, la religion et l'écologie. L'univers
+                                richement détaillé et les personnages complexes
+                                rendent la lecture captivante du début à la fin.
                             </Typo>
                         </div>
                     </div>
                 </div>
-                <Button variant="primary" classname="">
-                    Emprunter
-                </Button>
-            </div>
-        </Container>
+            </Container>
+        </>
+
+        // <div>
+        //     <Typo variant="para" color="other" className="mt-5 text-center">
+        //         * Les livres empruntés via la boîte aux livres sont à
+        //         retourner dans un délai de 30 jours.
+        //     </Typo>
+        // </div>
     );
 };
